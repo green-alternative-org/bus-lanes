b1abbe82785b6decd7e5805f7c81fe9f5c904471
green-alternative-org <141727459+green-alternative-org@users.noreply.github.com>
2023-08-18 19:55:57 +0400

    Update config.js

--- config.js
+++ config.js
@@ -40,10 +40,10 @@ var config = {
     {
       id: "bus-lanes-2018",
       alignment: "right",
-      title: "December, 2018",
+      title: "December 2018",
       image: "",
       description:
-        'At the end of 2018, the bus lane network covered around <span style="color: #9a0000;"><strong>10 kilometers</strong></span>. Nearly half of this network&#8217;s length was installed well before 2018.',
+        'At the end of 2018, the bus lane network covered around <span style="color: #9a0000;"><strong>10 kilometres</strong></span>. However, nearly half the length of the network had been installed well before 2018.',
       location: {
         center: [44.809, 41.733],
         zoom: 11.15,
@@ -66,10 +66,10 @@ var config = {
     {
       id: "bus-lanes-2019",
       alignment: "right",
-      title: "December, 2019",
+      title: "December 2019",
       image: "",
       description:
-        'Only a <span style="color: #9a0000;"><strong>675 meter-strech</strong></span> was added to the network.',
+        'Only a <span style="color: #9a0000;"><strong>675-metre stretch</strong></span> was added to the network. Comprehensive street redesign projects commenced on Chavchavadze Avenue and in the suburb of Gldani.',
       location: {
         center: [44.809, 41.733],
         zoom: 11.15,
@@ -100,10 +100,10 @@ var config = {
     {
       id: "bus-lanes-2020a",
       alignment: "right",
-      title: "December, 2020",
+      title: "December 2020",
       image: "",
       description:
-        'In 2020, City Hall added <span style="color: #9a0000;"><strong>18 kilometers</strong></span> of lanes to its network - the biggest annual increase. But the year also saw controversies. International consultants <a href="https://cdia.asia/wp-content/uploads/2023/03/GEO_TbilisiTransportDepartmentAdvisoryProject_Dec2020.pdf">noted</a> that before the 2020 parliamentary elections transport authorities deviated from their plan to create a continuous bus lane on a vital transit route. Instead, they decided to sporadically place bus lanes on wide streets without parking issues - a decision that highlights persistent short-term thinking hindering reforms.',
+        'City Hall added <span style="color: #9a0000;"><strong>18 kilometres</strong></span> of lanes to its network, the largest annual increase in a single year. But with parliamentary elections slated for late October 2020, political considerations started to affect the process. <a href="https://cdia.asia/wp-content/uploads/2023/03/GEO_TbilisiTransportDepartmentAdvisoryProject_Dec2020.pdf">According to a group of international consultants,</a> who advised City Hall and its Transport and Urban Development Agency (TUDA), even though the COVID-19 pandemic slowed implementation, between July 2020 and the start of the election campaign around early September, several bus lanes were either fully constructed or in the process of being constructed. In the run-up to the vote, however, transport authorities abandoned their initial commitment to provide a continuous bus lane on a key transit corridor to speed up buses on at least one arterial route. As the consultants noted, ‘the order of priorities was amended to focus on quick wins until after the election (for example, on wider streets with no parking problems)’.',
       location: {
         center: [44.809, 41.733],
         zoom: 11.15,
@@ -160,10 +160,10 @@ var config = {
     {
       id: "bus-lanes-2021",
       alignment: "right",
-      title: "December, 2021",
+      title: "December 2021",
       image: "",
       description:
-        'Only <span style="color: #9a0000;"><strong>5 km</strong></span> of bus lanes was added throughout 2021. Although City Hall has frequently cited the Covid-19 pandemic and accompanying challenges as a reason for the lag in progress, experts <a href="https://netgazeti.ge/opinion/638653/">argue</a> that the pandemic, in fact, presented a missed opportunity for implementing bus priority measures, as the short-term thinking is constantly interfering with the reforms.',
+        'After the parliamentary elections, the city braced itself for a local self-government vote in early October 2021. This had a knock-on effect on the expansion of the bus network, resulting in a mere <span style="color: #9a0000;"><strong>5-kilometre</strong></span> extension that year. Although City Hall has frequently cited the Covid-19 pandemic and accompanying challenges as a reason for the lack of progress, experts <a href="https://netgazeti.ge/opinion/638653/">argue</a> that the pandemic, in fact, presented a missed opportunity for implementing bus priority measures.',
       location: {
         center: [44.809, 41.733],
         zoom: 11.15,
@@ -194,10 +194,10 @@ var config = {
     {
       id: "bus-lanes-2022a",
       alignment: "right",
-      title: "December, 2022",
+      title: "December 2022",
       image: "",
       description:
-        'In 2022, a total of <span style="color: #9a0000;"><strong>14.5 km</strong></span> of bus lanes was added to the network.',
+        'The process regained momentum, with <span style="color: #9a0000;"><strong>14.5 kilometres</strong></span> of bus lanes added to the network.',
       location: {
         center: [44.809, 41.733],
         zoom: 11.15,
@@ -228,10 +228,10 @@ var config = {
     {
       id: "bus-lanes-2023",
       alignment: "right",
-      title: "July, 2023",
+      title: "July 2023",
       image: "",
       description:
-        'This year the bus lane network in Tbilisi has increased with an additional span of <span style="color: #9a0000;"><strong>13.5 km</strong></span>. Five years after making sustainable mobility a priority, City Hall has installed just over <b>55% (61 km)</b> of the designated bus lanes and in a way that is extremely fragmented, forming none of the proposed 10 arterial bus corridors to date. ',
+        'The bus lane network in Tbilisi increased by an additional <span style="color: #9a0000;"><strong>13.5 kilometres</strong></span>. led just over <b>55% (61 km)</b> of the designated bus lanes and in a way that is extremely fragmented, forming none of the proposed 10 arterial bus corridors to date. ',
       location: {
         center: [44.809, 41.733],
         zoom: 11.15,
@@ -308,7 +308,7 @@ var config = {
       title: "Future Expansion",
       image: "",
       description:
-        'The complete bus lane network is set to connect all the missing links to cover a system of some <span style="color: #9a0000;"><strong>110 km</strong></span> long.<p><div class="legend-scale"><ul class="legend-labels"><li><span style="background:#cd3333;"></span>Existing Routes</li><li><span style="background:#0e5c5d;"></span>Planned Routes</li></ul></div><p>',
+        'Five years after declaring public transport a priority, City Hall has only managed to install just over 55 per cent (61 kilometres) of the designated bus lanes. Overall, implementation of the project has been extremely fragmented and none of the proposed 10 arterial bus corridors have been established. Once complete, the TBT network should connect the fragmented lanes and cover some <span style="color: #9a0000;"><strong>110 kilometres/strong></span>. But if the process continues at the usual speed, it will take four more years before the whole network is finally established. In addition, recent official permission granted to certain categories of taxis to use bus lanes further reduces the speed of public transport and undermines the integrity of the bus lane system, not to mention the entire transport reform.<p><div class="legend-scale"><ul class="legend-labels"><li><span style="background:#cd3333;"></span>Existing Routes</li><li><span style="background:#0e5c5d;"></span>Planned Routes</li></ul></div><p>',
       location: {
         center: [44.809, 41.733],
         zoom: 11.15,
